8ca7df669b3705c521baf04fc594719f8f2bb82e
Javier Rugel <jrugel@gmail.com>
2023-05-22 00:00:43 -0400

    BKP

--- final-challenge.ps1
+++ final-challenge.ps1
@@ -3,13 +3,13 @@ function Exit-Log {
         [Parameter(Mandatory = $false)][string]$descripcionError
     )
 
-    Disconnect-AzAccount | Out-Null;
+    Disconnect-AzAccount
     $endTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss K"
-    Write-Output $descripcionError "Finalizando ejecución: $endTime"
+    Write-Output "`n$descripcionError" "Finalizando ejecución: $endTime`n"
     exit
 }
 
-Clear-Host
+# Clear-Host
 
 $beginTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss K"
 Write-Output "Iniciando ejecución: $beginTime"
@@ -17,41 +17,45 @@ Write-Output "Iniciando ejecución: $beginTime"
 if ($args.Count -ne 5) {
     $scriptName = $MyInvocation.MyCommand.Name
 
-    Write-Output "ERROR: Debe ejecutar el comando con todas los parámetros. Ej.:`n" "`t$scriptName <nombreUbicacion> <nombreResourceGroup> <nombreAppServicePlan> <nombreAppServiceWebApplication> <nombreAzureFunction>`n"
+    Write-Output "ERROR: Debe ejecutar el comando con todas los parámetros. Ej.:`n" "`t$scriptName <nombreUbicacion> <nombreResourceGroup> <nombreAppServicePlan> <nombreWebApplication> <nombreAzureFunction>`n"
     Exit-Log
 }
 
 $nombreUbicacion = $args[0]
 $nombreResourceGroup = $args[1]
 $nombreAppServicePlan = $args[2]
-$nombreAppServiceWebApplication = $args[3]
+$nombreWebApplication = $args[3]
 $nombreAzureFunction = $args[4]
 
 
 # Conectarme a Azure
-Disconnect-AzAccount | Out-Null;
+Disconnect-AzAccount
 try {
-    Connect-AzAccount    
+    Connect-AzAccount | Out-Null;
+    $ctx = Get-AzContext;
+    if ( $null -eq $ctx ) {
+        Exit-Log "No se pudo adquirir el contexto."
+    }
 }
 catch {
     Exit-Log "No se pudo conectar a Azure"
 }
 
 # Verificar Resource Group / Crear
-# Get-AzResourceGroup -Name $nombreResourceGroup -Location $nombreUbicacion -ErrorVariable noExisteResourceGroup -ErrorAction SilentlyContinue
+Get-AzResourceGroup -Name $nombreResourceGroup -Location $nombreUbicacion -ErrorVariable noExisteResourceGroup -ErrorAction SilentlyContinue
 
-# if ($noExisteResourceGroup) {
-#     try {
-#         Write-Output "Creando grupo de recursos $nombreResourceGroup en la ubicación $nombreUbicacion..."
-#         New-AzResourceGroup -Name $nombreResourceGroup -Location $nombreUbicacion
-#     }
-#     catch {
-#         Exit-Log "Ocurrió un error creando el grupo de recursos $nombreResourceGroup en la ubicación $nombreUbicacion"
-#     }
-# }
-# else {
-#     Exit-Log "Ya existe un grupo de recursos con el nombre $nombreResourceGroup."
-# }
+if ($noExisteResourceGroup) {
+    try {
+        Write-Output "Creando grupo de recursos $nombreResourceGroup en la ubicación $nombreUbicacion..."
+        New-AzResourceGroup -Name $nombreResourceGroup -Location $nombreUbicacion
+    }
+    catch {
+        Exit-Log "Ocurrió un error creando el grupo de recursos $nombreResourceGroup en la ubicación $nombreUbicacion"
+    }
+}
+else {
+    Exit-Log "Ya existe un grupo de recursos con el nombre $nombreResourceGroup."
+}
 
 
 # Verificar App Service Plan / Crear
@@ -60,7 +64,7 @@ Get-AzResource -Name $nombreAppServicePlan -ResourceGroupName $nombreResourceGro
 if ($noExisteAppServicePlan) {
     try {
         Write-Output "Creando el App Service plan $nombreAppServicePlan en $nombreResourceGroup..."
-        New-AzAppServicePlan -Name $nombreAppServicePlan -ResourceGroupName $nombreResourceGroup
+        $plan = New-AzAppServicePlan -Name $nombreAppServicePlan -ResourceGroupName $nombreResourceGroup -Location $nombreUbicacion
     }
     catch {
         Exit-Log "Hubo un error creando el App Service plan $nombreAppServicePlan en $nombreResourceGroup"
@@ -71,11 +75,39 @@ else {
 }
 
 # Verificar App Service Web Application / Crear
+Get-AzResource -Name $nombreWebApplication -ResourceGroupName $nombreResourceGroup -ResourceType Microsoft.Web/sites -ErrorVariable noExisteWebApplication -ErrorAction SilentlyContinue
+
+if ($noExisteWebApplication) {
+    try {
+        Write-Output "Creando Web Application $nombreWebApplication en $nombreResourceGroup..."
+        New-AzResource -Location $nombreUbicacion -ResourceName $nombreWebApplication -ResourceType "microsoft.Web/sites" -ResourceGroupName $nombreResourceGroup -Force -Properties @{
+            Createdby    = "AZPowershell"
+            serverFarmId = $sf.Id
+        }
+    }
+    catch {
+        Exit-Log "Hubo un error creando Web Application $nombreWebApplication en $nombreResourceGroup"
+    }
+}
+else {
+    Exit-Log "Ya existe una Web Application $nombreWebApplication en $nombreResourceGroup"
+}
 
 # Verificar Azure Function / Crear
+Get-AzResource -Name $nombreAzureFunction -ResourceGroupName $nombreResourceGroup -ResourceType Microsoft.Web/sites -ErrorVariable noExisteAzureFunction -ErrorAction SilentlyContinue
 
+if ($noExisteAzureFunction) {
+    try {
+        Write-Output "Creando Web Function $nombreAzureFunction en $nombreResourceGroup..."
+        
+        New-AzResource -Location $nombreUbicacion -Kind "functionapp" -Force -ResourceName $nombreAzureFunction -ResourceGroupName $nombreResourceGroup -ResourceType "microsoft.Web/sites" -Properties @{Createdby = "AZPowershell" }
+    }
+    catch {
+        Exit-Log "Hubo un error creando la función $nombreAzureFunction en $nombreResourceGroup"
+    }
+}
+else {
+    Exit-Log "Ya existe una función $nombreAzureFunction en $nombreResourceGroup"
+}
 
-
-Exit-Log
-
-
+Exit-Log "Finalizando sin errores..."
